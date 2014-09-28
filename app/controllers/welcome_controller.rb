180761419d4d1f14df3fe803dcb7940fde243ad0
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-09-28 18:29:13 -0400

    added foundation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    if params[:tag]
+      @insights = Insight.order(created_at: :desc).tagged_with(params[:tag])
+    else
+      @insights = Insight.order(created_at: :desc)
+    end
   end
 end
